e8ab95b6d865a916ee13ec69b97853347df84b00
SM ORIGINALS STUDIO <SMOS-Github@users.noreply.github.com>
2024-03-27 21:39:54 +0530

    Commit 2
    Attempt First Challenge!
    
    Complete

---
+++ TC2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /**Challenge 1 Complete */
+    int no{};
+    cout<<"Enter No Between 1 - 100 \nOK! What's Your favorite No : ";
+    cin>>no;
+    cout<<"Amazing! "<<no<< " Thats My Favorite No Too!"<<endl;
+}
