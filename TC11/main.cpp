9d47435b2172f098bd3d4b1f65fdd84d459e2b1e
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-09-29 21:16:13 +0530

    no info
    no info

--- TC11/main.cpp
+++ TC11/main.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main()
 {
-    int no=5;
-    int no2=3;
-    no=no-no2;
-    cout<<no<<endl;
+    for(int i=0;i<=10;i++)
+    {
+
+        char value;
+    cin>>value;
+    int x;
+    x=value;
+    cout<<x<<endl;
+    }
+
 }
